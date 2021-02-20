ce3fd48336371003e58213cbb41ce94ac430d693
Lition802 <79248548+Lition802@users.noreply.github.com>
2021-02-20 07:06:31 -0600

    Add files via upload

---
+++ MiraiAPI/MiraiAPI/Config.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Config
+    {
+        /// <summary>
+        /// websocket地址
+        /// </summary>
+        public string WebsocketHost { get; set; }
+        /// <summary>
+        /// websocket端口
+        /// </summary>
+        public int WebsocketPort { get; set; }
+        /// <summary>
+        /// 要登录的QQ号
+        /// </summary>
+        public uint QQ { get; set; }
+        /// <summary>
+        /// Mirai-Http-API的key
+        /// </summary>
+        public string authKey { get; set; }
+        /// <summary>
+        /// 消息转发的群号
+        /// </summary>
+        public List<int> Group { get; set; }
+    }
+    public class auth
+    {
+        public int code { get; set; }
+        public string session { get; set; }
+    }
+    public class receive
+    {
+        public int code { get; set; }
+        public string msg { get; set; }
+        public string messageId { get; set; }
+    }
+    public class verify
+    {
+        public string sessionKey { get; set; }
+        public uint qq { get; set; }
+    }
+    public class MessageChain
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string text { get; set; }
+    }
+
+    public class Msg
+    {
+        /// <summary>
+        /// 激活的sessionkey
+        /// </summary>
+        public string sessionKey { get; set; }
+        /// <summary>
+        /// 目标好友QQ号
+        /// </summary>
+        public uint target { get; set; }
+        /// <summary>
+        /// 引用一条消息的messageId进行回复
+        /// </summary>
+        public List<MessageChain> messageChain { get; set; }
+    }
+    public class recall
+    {
+        /// <summary>
+        /// 已经激活的Session
+        /// </summary>
+        public string sessionKey { get; set; }
+        /// <summary>
+        /// 需要撤回的消息的messageId
+        /// </summary>
+        public int target { get; set; }
+    }
+    public class mute
+    {
+        /// <summary>
+        /// 你的session key
+        /// </summary>
+        public string sessionKey { get; set; }
+        /// <summary>
+        /// 指定群的群号
+        /// </summary>
+        public uint target { get; set; }
+        /// <summary>
+        /// 指定群员QQ号
+        /// </summary>
+        public uint memberId { get; set; }
+        /// <summary>
+        /// 禁言时长，单位为秒，最多30天，默认为0
+        /// </summary>
+        public int time { get; set; }
+    }
+
+}
---
+++ MiraiAPI/MiraiAPI/MiraiRuntime.cs
@@ -0,0 +1,155 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace MiraiAPI
+{
+    public class MiraiAPI
+    {
+        public class Message
+        {
+            /// <summary>
+            /// 发送好友消息
+            /// </summary>
+            /// <param name="text">要发送的文本</param>
+            /// <param name="key">你的Session</param>
+            /// <param name="host">Http服务器地址</param>
+            /// <param name="port">Http服务器端口</param>
+            /// <param name="qq">好友QQ号</param>
+            public string sendFriendMessage(string host, int port, uint qq, string key, string text)
+            {
+                var m = new Data.MessageChain()
+                {
+                    type = "Plain",
+                    text = text
+                };
+                var msg = new List<Data.MessageChain>();
+                msg.Add(m);
+                var messgae = new Data.Msg()
+                {
+                    sessionKey = key,
+                    target = qq,
+                    messageChain = msg
+                };
+                string newmsg = JsonConvert.SerializeObject(messgae);
+                return HttpPost("http://" + host + ":" + port + "/sendFriendMessage", newmsg);
+            }
+            /// <summary>
+            /// 发送群聊消息
+            /// </summary>
+            /// <param name="text">要发送的文本</param>
+            /// <param name="key">你的Session</param>
+            /// <param name="host">Http服务器地址</param>
+            /// <param name="port">Http服务器端口</param>
+            /// <param name="qq">好友QQ号</param>
+            public string sendGroupMessage(string host, int port, uint groupqq, string key, string text)
+            {
+                var m = new Data.MessageChain()
+                {
+                    type = "Plain",
+                    text = text
+                };
+                var msg = new List<Data.MessageChain>();
+                msg.Add(m);
+                var messgae = new Data.Msg()
+                {
+                    sessionKey = key,
+                    target = groupqq,
+                    messageChain = msg
+                };
+                string newmsg = JsonConvert.SerializeObject(messgae);
+                return HttpPost("http://" + host + ":" + port + "/sendGroupMessage", newmsg);
+            }
+            /// <summary>
+            /// 发送临时消息
+            /// </summary>
+            /// <param name="text">要发送的文本</param>
+            /// <param name="key">你的Session</param>
+            /// <param name="host">Http服务器地址</param>
+            /// <param name="port">Http服务器端口</param>
+            /// <param name="qq">好友QQ号</param>
+            public string sendTempMessage(string host, int port, uint qq, string key, string text)
+            {
+                var m = new Data.MessageChain()
+                {
+                    type = "Plain",
+                    text = text
+                };
+                var msg = new List<Data.MessageChain>();
+                msg.Add(m);
+                var messgae = new Data.Msg()
+                {
+                    sessionKey = key,
+                    target = qq,
+                    messageChain = msg
+                };
+                string newmsg = JsonConvert.SerializeObject(messgae);
+                return HttpPost("http://" + host + ":" + port + "/sendTempMessage", newmsg);
+            }
+            /// <summary>
+            /// 撤回消息
+            /// </summary>
+            /// <param name="text">要发送的文本</param>
+            /// <param name="key">你的Session</param>
+            /// <param name="host">Http服务器地址</param>
+            /// <param name="port">Http服务器端口</param>
+            /// <param name="msgid">需要撤回的信息ID</param>
+            public string recall(string host, int port, string key, int msgid)
+            {
+                var a = new Data.recall()
+                {
+                    sessionKey = key,
+                    target = msgid
+                };
+                string newmsg = JsonConvert.SerializeObject(a);
+                return HttpPost("http://" + host + ":" + port + "/recall", newmsg);
+            }
+            /// <summary>
+            /// 禁言某人
+            /// </summary>
+            /// <param name="key">你的Session</param>
+            /// <param name="host">Http服务器地址</param>
+            /// <param name="port">Http服务器端口</param>
+            /// <param name="time">禁言时长</param>
+            /// <param name="qq">禁言对象的QQ号</param>
+            /// <param name="groupqq">禁言对象所在的群号</param>
+            public string mute(string host, int port, string key, uint qq, uint groupqq, int time)
+            {
+                var a = new Data.mute()
+                {
+                    memberId = qq,
+                    sessionKey = key,
+                    target = groupqq,
+                    time = time
+                };
+                string msg = JsonConvert.SerializeObject(a);
+                return HttpPost("http://" + host + ":" + port + "/mute", msg);
+            }
+        }
+        public static string HttpPost(string url, string body)
+        {
+            Encoding encoding = Encoding.UTF8;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.ContentType = "application/json";
+            byte[] buffer = encoding.GetBytes(body);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+        public static void Main()
+        {
+            //连接机器人需要自行实现
+            //我封装好了一些事件的API
+            //剩下的就尽情发挥想象吧
+            var bot = new Message();          
+        }
+    }
+}
